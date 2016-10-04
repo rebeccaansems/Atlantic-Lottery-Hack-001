b50e9ad531bf29f694bccac1500a05b082e01041
Rebecca Ansems <ansems_rebecca@hotmail.com>
2016-10-04 20:30:48 -0300

    Fixed hair/skin on people not appearing layered properly

--- Assets/Code/ChangeSprite.cs
+++ Assets/Code/ChangeSprite.cs
@@ -8,21 +8,25 @@ public class ChangeSprite : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        if (bodyPart.Equals("Hair"))
-        {
-            this.GetComponent<SpriteRenderer>().sprite = colorOptions[CharacterData.characterData[transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber,0]];
-        }
-        else if (bodyPart.Equals("Pants"))
+        if (bodyPart.Equals("Pants"))
         {
             this.GetComponent<SpriteRenderer>().sprite = colorOptions[CharacterData.characterData[transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber,1]];
+            this.GetComponent<SpriteRenderer>().sortingOrder = 2 * transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber;
         }
         else if (bodyPart.Equals("Shirt"))
         {
             this.GetComponent<SpriteRenderer>().sprite = colorOptions[CharacterData.characterData[transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber,2]];
+            this.GetComponent<SpriteRenderer>().sortingOrder = 2 * transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber;
         }
         else if (bodyPart.Equals("Skin"))
         {
             this.GetComponent<SpriteRenderer>().sprite = colorOptions[CharacterData.characterData[transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber,3]];
+            this.GetComponent<SpriteRenderer>().sortingOrder = 2 * transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber-1;
+        }
+        else if (bodyPart.Equals("Hair"))
+        {
+            this.GetComponent<SpriteRenderer>().sprite = colorOptions[CharacterData.characterData[transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber, 0]];
+            this.GetComponent<SpriteRenderer>().sortingOrder = 2*transform.root.GetComponent<CharacterSpecificData>().currentCharacterNumber+1;
         }
     }
 }
