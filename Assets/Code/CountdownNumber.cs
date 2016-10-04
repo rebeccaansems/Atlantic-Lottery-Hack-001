44284efe991e9b9c625b9c3e2e86773c48d2d5f5
Rebecca Ansems <ansems_rebecca@hotmail.com>
2016-10-04 18:36:58 -0300

    Added functionality to main button and countdown scene

---
+++ Assets/Code/CountdownNumber.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CountdownNumber : MonoBehaviour
+{
+    public Text number;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.fixedTime == 1)
+        {
+            number.text = "5";
+        }
+        else if (Time.fixedTime == 2)
+        {
+            number.text = "4";
+        }
+        else if (Time.fixedTime == 3)
+        {
+            number.text = "3";
+        }
+        else if (Time.fixedTime == 4)
+        {
+            number.text = "2";
+        }
+        else if (Time.fixedTime == 5)
+        {
+            number.text = "1";
+        }
+        else if (Time.fixedTime == 6)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
---
+++ Assets/Code/SceneSwitcher.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	public void MainMenuPlayButtonPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
